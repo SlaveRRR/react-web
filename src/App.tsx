dba8941ce2a7bb0666092f3636284e7db5b15924
Slava-beginner <slava.arhipov40020141@gmail.com>
2023-11-29 14:00:48 +0300

    added limit,loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,24 @@
-import React,{FC} from 'react'
+import React, { FC, useEffect, useState } from 'react'
 
 import './App.css'
-import { Table,Space,Tag } from 'antd'
+import { Table, Space, Tag, Flex } from 'antd'
 import type { ColumnsType } from 'antd/es/table';
+import axios from 'axios';
+
+const LIMIT = 10
 
 interface DataType {
-  id:number;
+  id: number;
   key: React.Key;
-  name: string;
+  username: string;
   age: number;
   address: string;
-  gender:'male' | 'female';
-  tags: string[];
+  gender: 'male' | 'female';
+  eyeColor:string ;
 }
 
-const dataSource : DataType[] = [
-  {
-    key: '1',
-    name: 'Mike',
-    age: 32,
-    id:1,
-    gender:'male',
-    address: '10 Downing Street',
-    tags: ['nice'],
-  },
-  {
-    key: '2',
-    name: 'John',
-    age: 42,
-    id:2,
-    gender:'male',
-    address: '10 Downing Street',
-    tags: ['developer'],
-  },
-  {
-    key: '3',
-    name: 'Sam',
-    age: 42,
-    id:3,
-    gender:'male',
-    address: '10 Downing Street',
-    tags: ['teacher', 'developer'],
-  },
-  {
-    key: '4',
-    name: 'Olga',
-    age: 18,
-    id:4,
-    gender:'female',
-    address: '10 Downing Street',
-    tags: ['nice', 'loser'],
-  },
-  {
-    key: '5',
-    name: 'Alex',
-    age: 50,
-    id:5,
-    gender:'male',
-    address: '10 Avenue Stree',
-    tags: ['nice', 'developer'],
-  },
-];
+
+
 const columns: ColumnsType<DataType> = [
   {
     title: 'Id',
@@ -68,9 +26,9 @@ const columns: ColumnsType<DataType> = [
     key: 'id',
   },
   {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
+    title: 'Username',
+    dataIndex: 'username',
+    key: 'username',
     render: (text) => <a>{text}</a>,
   },
   {
@@ -87,24 +45,17 @@ const columns: ColumnsType<DataType> = [
     title: 'Address',
     dataIndex: 'address',
     key: 'address',
+    render: (value) => value.address
   },
   {
-    title: 'Tags',
-    key: 'tags',
-    dataIndex: 'tags',
-    render: (_, { tags }) => (
+    title: 'EyeColor',
+    key: 'eyeColor',
+    dataIndex: 'eyeColor',
+    render: (_, { eyeColor }) => (
       <>
-        {tags.map((tag) => {
-          let color = tag.length > 5 ? 'geekblue' : 'green';
-          if (tag === 'loser') {
-            color = 'volcano';
-          }
-          return (
-            <Tag color={color} key={tag}>
-              {tag.toUpperCase()}
-            </Tag>
-          );
-        })}
+       <Tag color={eyeColor == 'Amber' ? 'blue' : eyeColor} >
+              {eyeColor.toUpperCase()}
+        </Tag>
       </>
     ),
   },
@@ -113,7 +64,7 @@ const columns: ColumnsType<DataType> = [
     key: 'action',
     render: (_, record) => (
       <Space size="middle">
-        <a>Invite {record.name}</a>
+        <a>Invite {record.username}</a>
         <a>Delete</a>
       </Space>
     ),
@@ -124,9 +75,35 @@ const columns: ColumnsType<DataType> = [
 
 
 const App: FC = () => {
-  return <>
-  <Table  dataSource={dataSource} columns={columns} />
-  </>
+
+  const [offset, setOffset] = useState<number>(0)
+  const [dataSource, setDataSource] = useState<DataType[]>();
+  const [isLoading,setLoading] = useState<boolean>(false)
+  const getData = async (offset : number,limit:number)  =>{ 
+    setLoading(true)
+    const response = await axios.get(`https://dummyjson.com/users?limit=10&skip=${offset}&select=username,age,id,gender,eyeColor,address`)
+    const {data:{users}} = response;
+    console.log(response)
+    setDataSource(users)
+    setLoading(false)
+   
+  }
+  useEffect(() =>{
+    getData(offset,LIMIT)
+  },[offset])
+
+  return (
+    <>
+      <Table dataSource={dataSource} columns={columns} loading={isLoading} pagination={false} />
+      <Flex gap="middle" justify='center'>
+        <button onClick={() => setOffset(offset - 10)} disabled={offset == 0}>Назад</button>
+        <p>{offset === 0 ? 1 : (offset/10) + 1}</p>
+        <button disabled={offset == 90} onClick={() => setOffset(offset + 10)}>Вперёд</button>
+      </Flex>
+
+    </>
+  )
+
 }
 export default App
 
