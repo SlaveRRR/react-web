625d1330472e9e614a8b096c76c189e0d5aafec6
Вячеслав Архипов <slava.arhipov40020141@gmail.com>
2024-04-03 09:33:37 +0300

    added esling prettier husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,47 @@
-import React, { FC, useContext } from 'react'
+import React, { FC, useContext } from "react";
 
-
-import NavBar from './components/NavBar';
-import MainRouter from './app/routing';
-import { ConfigProvider } from 'antd';
-import { ctx } from './context';
-import GlobalStyles from './global-styles';
-
+import NavBar from "./components/NavBar";
+import MainRouter from "./app/routing";
+import { ConfigProvider } from "antd";
+import { ctx } from "./context";
+import GlobalStyles from "./global-styles";
 
 const App: FC = () => {
+   const { theme } = useContext(ctx);
 
-  const {theme} = useContext(ctx)
+   return (
+      <>
+         <ConfigProvider
+            theme={{
+               components: {
+                  Menu: {
+                     algorithm: true,
+                     ...theme.Menu,
+                  },
+                  Typography: {
+                     algorithm: true,
+                     ...theme.Typography,
+                  },
+                  Divider: {
+                     algorithm: true,
+                     ...theme.Divider,
+                  },
+                  Statistic: {
+                     algorithm: true,
 
-  return (
-
-    <>
-      <ConfigProvider
-      theme={{
-        components:{
-          Menu:{
-            algorithm:true,
-            ...theme.Menu
-          },
-          Typography:{
-            algorithm:true,
-            ...theme.Typography
-          },
-          Divider:{
-            algorithm:true,
-            ...theme.Divider
-          },
-          Statistic:{
-            algorithm:true,
-          
-            ...theme.Statistic
-          },
-          Avatar:{
-            colorTextPlaceholder:'#636363'
-          }
-         
-        }
-      }}
-      >
-        <NavBar />
-        <MainRouter />
-      </ConfigProvider>
-      <GlobalStyles mode={theme.mode}/>
-
-    </>
-  )
-
-}
-export default App
-
-
+                     ...theme.Statistic,
+                  },
+                  Avatar: {
+                     colorTextPlaceholder: "#636363",
+                  },
+               },
+            }}
+         >
+            <NavBar />
+            <MainRouter />
+         </ConfigProvider>
+         <GlobalStyles mode={theme.mode} />
+      </>
+   );
+};
+export default App;
